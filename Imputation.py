5e64e367097cafd0a2d2c2b61aad780de099f88c
c-shekhar <chandrashekhar1503@gmail.com>
2018-02-11 21:23:04 +0530

    Class for data imputaion

---
+++ Imputation.py
@@ -0,0 +1,10 @@
+class ImputeData():
+
+	def __init__(self):
+		pass
+
+	def impute_categorical_features(self, categ_features_list, groupby_key):
+		pass
+
+	def impute_continuous_features(self, cont_features_list, groupby_key):
+		pass
