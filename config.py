e60554692ac3078d65083e2c5092ce67f485324d
c-shekhar <chandrashekhar1503@gmail.com>
2018-02-11 21:20:32 +0530

    configuration file --WIP

---
+++ config.py
@@ -0,0 +1,12 @@
+import numpy as np
+empty_notations = {
+					'' : np.nan,
+					'nan' : np.nan,
+					'N/A' : np.nan,
+					'NONE' : np.nan,
+					'NA' : np.nan
+				  }
+
+numeric_data_types = ['int64','float64']
+
+object_data_types = ['object']
